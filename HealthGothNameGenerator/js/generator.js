6ccefeb8e9a346d0b337d07ea98e4815e217bc5d
Corey Davenport <cairnelan@gmail.com>
2015-04-28 15:38:14 -0400

    Changes

--- HealthGothNameGenerator/js/generator.js
+++ HealthGothNameGenerator/js/generator.js
@@ -1,10 +1,6 @@
 
-  /*  Online Name Generators v.1.0 script is  made by Niels Gamborg webmaster at online-generator.com.  
-  *    The script is  free (as in "free speech" and also as in "free beer") in anyway. Use it after you own likings.
-  *    Peace and love. :)
-  */  
     function generator(){
-      // Add your own words to the wordlist. Be carefull to obey the showed syntax
+      
       
       var wordlist1 = ["Sad","Adi","Cyber","Magic","Micro","Aero","Future","HyperSport","White","Bio","Godidas","Hydrator","Baby","Golden","Silver","Hydra", "Lyte", "Dead",
               "Blaqk","Dark","Black", "Under", "Speed","Jog","Little","Advanced","OXY","Energy", "Supra","Night","HyPerFlex","Tech","Pro","Plastic"]
