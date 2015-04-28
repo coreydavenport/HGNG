2db39aa75a9044c4ca5263263f42d8173d7d0f5e
Corey Davenport <cairnelan@gmail.com>
2015-04-28 15:01:23 -0400

    bug fix

--- HealthGothNameGenerator/js/generator.js
+++ HealthGothNameGenerator/js/generator.js
@@ -6,12 +6,11 @@
     function generator(){
       // Add your own words to the wordlist. Be carefull to obey the showed syntax
       
-      var wordlist1 = ["Sad","Adi","Cyber","Magic","Micro","Aero","Future","HyperSport",
-              "White","Bio","Godidas","Hydrator","Baby","Golden","Silver","Hydra" "Lyte", "Dead",
+      var wordlist1 = ["Sad","Adi","Cyber","Magic","Micro","Aero","Future","HyperSport","White","Bio","Godidas","Hydrator","Baby","Golden","Silver","Hydra" "Lyte", "Dead",
               "Blaqk", "Under", "Speed"]
       
       var wordlist2 = ["Knife","Prosthesia","Android","Widow","Runner","Fades","LTD",
-              "Air","Bullet","Tear","Foot","Hand" "God" "Metal"]
+              "Air","Bullet","Tear","Foot","Hand", "God", "Metal"]
       
       // Random numbers are made 
       var randomNumber1 = parseInt(Math.random() * wordlist1.length);
